fefbb296f7cc0a2e84effcde6b63551b9f765ad4
eliudkaruga <eliudkaruga97@gmail.com>
2024-10-24 18:53:52 +0300

    updated font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import Navbar from '@/components/navbar';
 
-const inter = Inter({ subsets: ['latin'] });
+
 
 export const metadata: Metadata = {
   title: 'Inventory Management System',
@@ -19,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
